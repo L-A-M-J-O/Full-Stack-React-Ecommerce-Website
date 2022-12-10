1a8a211e0764c1d322771daaa16dd85a267a75d4
L-A-M-J-O <lamjo30@gmail.com>
2022-12-10 17:01:24 +0100

    hehe

--- my-app/src/components/Footer/Footer.jsx
+++ my-app/src/components/Footer/Footer.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import icon from '../../assets/img/icon.png';
+
+import { Container, Row, Col, ListGroup, ListGroupItem} from 'reactstrap';
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <Container>
+        <Row>
+          <Col lg='4'>
+          <div className='logo'>
+              <img src={icon} alt='logo'/>
+              <div>
+                <h1>KANBUN</h1> 
+              </div>
+          </div>
+          <p className='footer__text'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, tenetur.
+          </p>
+          </Col>
+          <Col lg='3'>
+            <div className='footer__quick-links'>
+              <h4 className='quick__links-title'>Top Categoria</h4>
+              <ListGroup className='mb-3'>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='#'>Mobile Phones</Link>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='#'>Mobile Phones</Link>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='#'>Mobile Phones</Link>
+                </ListGroupItem>
+                <ListGroupItem className='ps-0 border-0'>
+                  <Link to='#'>Mobile Phones</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg='2'></Col>
+          <Col lg='3'></Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
